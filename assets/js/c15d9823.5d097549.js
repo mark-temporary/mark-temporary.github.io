d1828546c2fd07db19b5ff081c97bd34ebf71adc
markusbkk <happyferret2022@gmail.com>
2026-01-02 18:36:05 +0100

    Deploy website - based on 4a6d8129324f273ffe6defd9139004b9a193a7a4

--- assets/js/c15d9823.5d097549.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkhappy_ferret_company=globalThis.webpackChunkhappy_ferret_company||[]).push([[146],{9328(a){a.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
