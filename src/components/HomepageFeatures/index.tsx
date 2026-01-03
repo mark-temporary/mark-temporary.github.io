dc7a7e8aa2e82b2e6ea48d23ecd1da8f64451ba6
markusbkk <happyferret2022@gmail.com>
2026-01-03 02:33:26 +0100

    Update landing page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -5,48 +5,60 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  // Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Powered by a love for retro',
+    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        As children of the 90s, we all love and fondly remember
+        the era of 16-bit and MS DOS, and we seek to replicate
+        this both on retro gaming hardware and modern systems.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Focused on whimsical multiplatform games',
+    // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Whether a phone, modern desktop computer or any number
+        of retro consoles or home computers is your weapon of choice, 
+        we believe everybody deserves to enjoy
+        games on their favorite platform.
+        <br/><br/>
+        For this purpose we vow to do our best to create cozy,
+        memorable gaming experiences for anything from Atari 2600,
+        over N64 and Dreamcast, all the way to modern consoles and
+        PCs.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Utilizing custom technology',
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Being focused on resource efficient and well optimized experiences,
+        you won't find the likes of Unreal Engine 5 here.
+
+        Most of Happy Ferret's upcoming titles will use one of three
+        in-house engines that vary in features, graphical fidelity
+        and platform support.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
